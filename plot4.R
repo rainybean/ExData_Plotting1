4e12c21beeb1bd11957297465ae5ec6d82e84847
rainybean <rainybean@gmail.com>
2014-05-09 17:55:20 +0800

    message

--- plot4.R
+++ plot4.R
@@ -15,9 +15,10 @@ data.s <- with(data,subset(data,Date>="2007-02-01"&Date<"2007-02-03"))
 data.s$Datetime <- as.POSIXct(paste(data.s$Date,data.s$Time)) 
 
 #plot line chart & save to a png file (set time locale to English for the x-axis labels)
-par(mfrow=c(2,2),mar=c(4,4,2,1))
 Sys.setlocale(category = "LC_TIME", locale = "C")
 
+png("plot4.png",width=480,height=480)
+par(mfrow=c(2,2),mar=c(4,4,2,1))
 #1
 with(data.s,plot(Datetime,Global_active_power,type="l",xlab="",ylab="Global Active Power"))
 #2
@@ -29,11 +30,7 @@ with(data.s,lines(Datetime,Sub_metering_3,col="blue"))
 legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n",inset=0)
 #4
 with(data.s,plot(Datetime,Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power"))
-
-dev.copy(png,file="plot4.png",width=480,height=480)
 dev.off()
 
 Sys.setlocale(category = "LC_ALL", locale = "")
-
-
 
